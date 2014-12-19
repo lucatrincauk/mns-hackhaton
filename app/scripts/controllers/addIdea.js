6bf04245786f1323b04c6399b1def9f5b152eac4
C1882300_Anthony_Weber <c1882300@MB469.local>
2014-12-19 12:49:19 +0000

    fixed bug that didn't add colour and pattern anymore

--- app/scripts/controllers/addIdea.js
+++ app/scripts/controllers/addIdea.js
@@ -59,7 +59,7 @@ angular.module('mnsHackhatonApp')
 		$scope.setColour = function() {
 			$scope.colour = $scope.colorpicker.colour;
 			$scope.pattern = '';
-			$scope.factoryShape.draw(1, $scope.colorpicker.colour, $scope.pattern);
+			$scope.factoryShape.draw(1, $scope.shape.id, $scope.colorpicker.colour, $scope.pattern);
 		};
 
 		/**
@@ -68,7 +68,7 @@ angular.module('mnsHackhatonApp')
 		$scope.setPattern = function(pattern) {
 			$scope.pattern = pattern;
 			$scope.colour = '';
-			$scope.factoryShape.draw(1, $scope.colorpicker.colour, $scope.pattern);
+			$scope.factoryShape.draw(1, $scope.shape.id, $scope.colorpicker.colour, $scope.pattern);
 		};
 
 
